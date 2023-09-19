a4866ac313de53ba6361c00857821ef7d2a84e8b
lroberto68 <50851016+lroberto68@users.noreply.github.com>
2023-09-19 23:04:00 +0000

    updated message

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from jinja2.environment import TemplateStream
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p><b>Hello, darlings!</b></p>"
+    return "<p><b>Hello, amazons!</b></p>"
 
 if  __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
